34c659fa39ed7661d2ca72b9a07046f7a46e2dde
Ted Francis <tedfrancis@MacBook-Pro.local>
2020-11-06 14:25:40 -0500

    Changed Link

--- app/javascript/react/components/UserLeagueTile.js
+++ app/javascript/react/components/UserLeagueTile.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom"
 
 const UserLeagueTile = (props) => {
   return (
-      <div>
-        <Link to={`leagues/${props.id}`} >
-          {props.league_name}
-        </Link>
-        <p>{props.location}</p>
-      </div>
+    <div>
+      <Link to={`/leagues/${props.id}`} >
+        {props.league_name}
+      </Link>
+      <p>{props.location}</p>
+    </div>
   )
 }
 
